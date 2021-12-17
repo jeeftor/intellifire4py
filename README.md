71ecd07b2230cf144dbd4c37f17e057a34574684
Jeff Stein <jstein@mitre.org>
2021-12-17 13:21:28 -0700

    feat: added more details

---
+++ README.md
@@ -0,0 +1,19 @@
+# Intellifire 
+
+Intellifire is a wifi module for a variety of fireplaces. It has both ios/android apps - but they dont like to publish the api.
+
+From my research I've identified 4 endpoints:
+
+`/poll`
+`/get_serial`
+`/get_challenge`
+`/post`
+
+This module will parse data from botht he `/get_serial` and `/poll` endpoints and parse the resultatn JSON into something readable.
+
+If anybody knows more about OAuth and wants to help me reverse engineer the control endpoints I'd love the help!
+
+
+# Usage
+
+(tbd)
---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="intellifire4py", # Replace with your own username
+    version="0.1.1",
+    author="Jeef",
+    author_email="",
+    description="An API to read the status of an intellifire wifi module",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jeeftor/intellifire4py",
+    packages=setuptools.find_packages(),
+    install_requires=[''],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
