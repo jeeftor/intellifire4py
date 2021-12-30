3cd91986fbd6003eba9f51f286c1d5c21cfdb467
Jeff Stein <jstein@mitre.org>
2021-12-30 11:49:33 -0700

    feat: bumped version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="intellifire4py", # Replace with your own username
-    version="0.1",
+    version="0.1.0",
     author="Jeef",
     author_email="",
     license="MIT",
