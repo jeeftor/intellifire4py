9789bbda4375ebcfdc7fd3bc75cf029d8a9575e2
Jeff Stein <jstein@mitre.org>
2022-01-02 19:18:03 -0700

    feat: 0.2.4

--- intellifire4py/intellifire.py
+++ intellifire4py/intellifire.py
@@ -1,14 +1,6 @@
-import json
 import requests
 
 from intellifire4py.intellifirepolldata import IntellifirePollData
-
-class AsyncIntellifire:
-    def __init__(self, ip) -> None:
-        self.ip = ip
-
-        self.__data: IntellifirePollData = None
-
 
 
 class Intellifire:
@@ -19,9 +11,16 @@ class Intellifire:
         self.__data: IntellifirePollData = None
 
     def poll(self):
-        response = requests.get("http://" + self.ip + "/poll")
-        print(response.json())
-        self.__data = IntellifirePollData(**response.json())
+        try:
+            response = requests.get("http://" + self.ip + "/poll")
+            if response.status_code == 404:
+                # Valid address - but poll endpoint not found
+                raise ConnectionError("Fireplace Endpoint Not Found - 404")
+
+            print(response.json())
+            self.__data = IntellifirePollData(**response.json())
+        except ConnectionError as e:
+            raise ConnectionError("ConnectionError - host not found")
 
     @property
     def data(self) -> IntellifirePollData:
@@ -33,8 +32,6 @@ def main():
     fire = Intellifire("192.168.1.80")
     # Poll the fire
     fire.poll()
-
-
     print(f"{fire.data.temperature_c} c")
     print(f"{fire.data.temperature_f} f")
 
