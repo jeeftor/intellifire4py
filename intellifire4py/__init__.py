588dd00ceb8517d9e10966567c915468ea1cab4c
Jeff Stein <jstein@mitre.org>
2022-01-03 10:01:04 -0700

    Auto Commit for version [0.3.1]

--- intellifire4py/__init__.py
+++ intellifire4py/__init__.py
@@ -1,4 +1,4 @@
 
 from intellifire4py.intellifire import Intellifire
-from intellifire_async import IntellifireAsync
+from intellifire4py.intellifire_async import IntellifireAsync
 from intellifire4py.intellifirepolldata import IntellifirePollData
