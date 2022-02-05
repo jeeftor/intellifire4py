de543826bbcb083dca38b9a0e8acf816a2955e46
Jeef <jeeftor@users.noreply.github.com>
2022-02-04 21:32:55 -0700

    Auto Commit for version [0.6]

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="intellifire4py", # Replace with your own username
-    version="0.5.1",
+    version="0.6",
     author="Jeef",
     author_email="",
     license="MIT",
