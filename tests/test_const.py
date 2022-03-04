609f93b1dc12bb04b296478e19f89d4d3e18244f
Jeef <jeeftor@users.noreply.github.com>
2022-03-04 14:52:47 -0700

    Pre-CI

--- tests/test_const.py
+++ tests/test_const.py
@@ -1,15 +1,13 @@
+"""Basic error code test case."""
 from unittest import TestCase
 
 from intellifire4py.const import IntellifireErrorCode
 
 
 class TestErrors(TestCase):
+    """Test Case."""
 
     def test_errors(self):
-
-
+        """Test Function."""
         e = IntellifireErrorCode(642)
-        assert (e == IntellifireErrorCode.OFFLINE)
-
-
-        # assert(IntellifireErrorString.from_code(642) == IntellifireErrorString.OFFLINE)
+        assert e == IntellifireErrorCode.OFFLINE
--- tests/test_model.py
+++ tests/test_model.py
@@ -1,21 +1,15 @@
-import json
-from unittest import TestCase
-
+"""Test File."""
 from pydantic import ValidationError
 
 from intellifire4py import IntellifirePollData
-from intellifire4py.const import IntellifireErrorCode
 
 
 def test_json_files():
-
-    for file_name in ['test1.json', 'error_6.json']:
+    """Test Function."""
+    for file_name in ["test1.json", "error_6.json"]:
         file = open(file_name)
         json_str = file.read()
         try:
-            p = IntellifirePollData.parse_raw(json_str)
+            IntellifirePollData.parse_raw(json_str)
         except ValidationError as e:
             print(e)
-
-
-
