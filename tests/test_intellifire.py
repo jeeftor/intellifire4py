be723ffe98ab2c004deab3c4e184757d5d3a905b
Jeef <jeeftor@users.noreply.github.com>
2022-06-03 13:03:57 -0600

    testing

--- tests/test_intellifire.py
+++ tests/test_intellifire.py
@@ -1,118 +1,70 @@
-"""Test Case."""
-import json
-from unittest import TestCase
+"""Intellifire Tester."""
+from unittest.mock import AsyncMock
 
-from intellifire4py import IntellifirePollData
-from intellifire4py.const import IntellifireErrorCode
+import pytest
 
-JSON1 = """
-    {
-  "name": "",
-  "serial": "9CE2E834CE109D849CBB15CDDBAFF381",
-  "temperature": 18,
-  "battery": 0,
-  "pilot": 1,
-  "light": 0,
-  "height": 0,
-  "fanspeed": 1,
-  "hot": 0,
-  "power": 1,
-  "thermostat": 0,
-  "setpoint": 2100,
-  "timer": 0,
-  "timeremaining": 0,
-  "prepurge": 0,
-  "feature_light": 0,
-  "feature_thermostat": 1,
-  "power_vent": 0,
-  "feature_fan": 1,
-  "errors": [
-    6,642
-  ],
-  "fw_version": "0x00030100",
-  "fw_ver_str": "0.3.1+hw2",
-  "downtime": 0,
-  "uptime": 648,
-  "connection_quality": 984864,
-  "ecm_latency": 0,
-  "ipv4_address": "192.168.1.80"
-}"""
-
-d1 = json.loads(JSON1)
-d2 = {
-    "name": "",
-    "serial": "9CE2E834CE109D849CBB15CDDBAFF381",
-    "temperature": 18,
-    "battery": 0,
-    "pilot": 1,
-    "light": 0,
-    "height": 0,
-    "fanspeed": 1,
-    "hot": 0,
-    "power": 1,
-    "thermostat": 0,
-    "setpoint": 2100,
-    "timer": 0,
-    "timeremaining": 0,
-    "prepurge": 0,
-    "feature_light": 0,
-    "feature_thermostat": 1,
-    "power_vent": 0,
-    "feature_fan": 1,
-    "errors": [],
-    "fw_version": "0x00030100",
-    "fw_ver_str": "0.3.1+hw2",
-    "downtime": 0,
-    "uptime": 840,
-    "connection_quality": 987104,
-    "ecm_latency": 0,
-    "ipv4_address": "192.168.1.80",
-}
+from intellifire4py.intellifire import IntellifireAPILocal
 
 
-class TestIntellifire(TestCase):
-    """Test case."""
+@pytest.fixture()
+def mock_send_command(mocker):  # type: ignore
+    """Mock send command."""
+    async_mock = AsyncMock()
+    mocker.patch(
+        "intellifire4py.intellifire.IntellifireAPILocal.send_command",
+        side_effect=async_mock,
+    )
+    return async_mock
 
-    # def test_model_fields(self) -> None:
-    #     """Test fields in the model."""
-    #     d = IntellifirePollData.parse_obj(d1)
 
-    # init_data_dict = d.dict()
+@pytest.fixture()
+def mock_get_challenge(mocker):  # type: ignore
+    """Mock get challenge."""
+    async_mock = AsyncMock()
+    mocker.patch(
+        "intellifire4py.intellifire.IntellifireAPILocal._get_challenge",
+        side_effect=async_mock,
+    )
+    return async_mock
 
-    # for data in IntellifireCommand:
-    #     name = data.name
-    #
-    #     field_name: str = data.value['model_field']
-    #     new_dict = {**d.dict(), field_name: 1}
-    #     d.parse_obj(new_dict)
-    #     assert d.dict()[field_name] == 1
 
-    def test_poll(self) -> None:
-        """Test Parsing."""
-        print(d2)
-        print(d1)
+@pytest.fixture()
+def mock_background_poll(mocker):  # type: ignore
+    """Mock poll."""
+    async_mock = AsyncMock()
+    mocker.patch(
+        "intellifire4py.intellifire.IntellifireAPILocal.__background_poll",
+        side_effect=async_mock,
+    )
+    return async_mock
 
-        try:
-            d = IntellifirePollData.parse_obj(d1)
-            assert d.error_codes[0] == IntellifireErrorCode.FAN_DELAY
-            assert d.error_codes[1] == IntellifireErrorCode.OFFLINE
 
-            assert ", ".join([x.name for x in d.error_codes]) == "FAN_DELAY, OFFLINE"
+@pytest.mark.asyncio
+async def test_send_local(mock_send_command, mock_background_poll):  # type: ignore
+    """Test local command sending."""
+    api = IntellifireAPILocal(fireplace_ip="192.168.1.5")
+    await api.pilot_on()
+    assert api.data.pilot_on is True
+    await api.pilot_off()
+    assert api.data.pilot_on is False
+    await api.flame_on()
+    assert api.data.is_on is True
+    await api.flame_off()
+    assert api.data.is_on is False
+    await api.set_sleep_timer(minutes=60)
+    assert api.data.timeremaining_s == 60 * 60
+    await api.set_sleep_timer(minutes=10)
+    assert api.data.timeremaining_s == 60 * 10
 
-            assert (
-                d.error_codes_string
-                == ", ".join([x.name for x in d.error_codes])
-                == "FAN_DELAY, OFFLINE"
-            )
+    await api.set_lights(level=1)
+    assert api.data.light_level == 1
+    await api.set_flame_height(height=1)
+    assert api.data.flameheight == 1
+    await api.set_fan_speed(speed=1)
+    assert api.data.fanspeed == 1
 
-            assert d.error_fan_delay is True
-            assert d.error_offline is True
-            assert d.error_accessory is False
 
-        except:  # noqa: E722
-            self.fail("Couldn't parse D1")
-
-        try:
-            IntellifirePollData.parse_obj(d2)
-        except:  # noqa: E722
-            self.fail("Couldn't parse D2")
+def test_needs_login() -> None:
+    """Login test."""
+    api = IntellifireAPILocal(fireplace_ip="192.168.1.5")
+    assert api._needs_login() is True
---
+++ tests/test_intellifire_random.py
@@ -0,0 +1,118 @@
+"""Test Case."""
+import json
+from unittest import TestCase
+
+from intellifire4py import IntellifirePollData
+from intellifire4py.const import IntellifireErrorCode
+
+JSON1 = """
+    {
+  "name": "",
+  "serial": "9CE2E834CE109D849CBB15CDDBAFF381",
+  "temperature": 18,
+  "battery": 0,
+  "pilot": 1,
+  "light": 0,
+  "height": 0,
+  "fanspeed": 1,
+  "hot": 0,
+  "power": 1,
+  "thermostat": 0,
+  "setpoint": 2100,
+  "timer": 0,
+  "timeremaining": 0,
+  "prepurge": 0,
+  "feature_light": 0,
+  "feature_thermostat": 1,
+  "power_vent": 0,
+  "feature_fan": 1,
+  "errors": [
+    6,642
+  ],
+  "fw_version": "0x00030100",
+  "fw_ver_str": "0.3.1+hw2",
+  "downtime": 0,
+  "uptime": 648,
+  "connection_quality": 984864,
+  "ecm_latency": 0,
+  "ipv4_address": "192.168.1.80"
+}"""
+
+d1 = json.loads(JSON1)
+d2 = {
+    "name": "",
+    "serial": "9CE2E834CE109D849CBB15CDDBAFF381",
+    "temperature": 18,
+    "battery": 0,
+    "pilot": 1,
+    "light": 0,
+    "height": 0,
+    "fanspeed": 1,
+    "hot": 0,
+    "power": 1,
+    "thermostat": 0,
+    "setpoint": 2100,
+    "timer": 0,
+    "timeremaining": 0,
+    "prepurge": 0,
+    "feature_light": 0,
+    "feature_thermostat": 1,
+    "power_vent": 0,
+    "feature_fan": 1,
+    "errors": [],
+    "fw_version": "0x00030100",
+    "fw_ver_str": "0.3.1+hw2",
+    "downtime": 0,
+    "uptime": 840,
+    "connection_quality": 987104,
+    "ecm_latency": 0,
+    "ipv4_address": "192.168.1.80",
+}
+
+
+class TestIntellifire(TestCase):
+    """Test case."""
+
+    # def test_model_fields(self) -> None:
+    #     """Test fields in the model."""
+    #     d = IntellifirePollData.parse_obj(d1)
+
+    # init_data_dict = d.dict()
+
+    # for data in IntellifireCommand:
+    #     name = data.name
+    #
+    #     field_name: str = data.value['model_field']
+    #     new_dict = {**d.dict(), field_name: 1}
+    #     d.parse_obj(new_dict)
+    #     assert d.dict()[field_name] == 1
+
+    def test_poll(self) -> None:
+        """Test Parsing."""
+        print(d2)
+        print(d1)
+
+        try:
+            d = IntellifirePollData.parse_obj(d1)
+            assert d.error_codes[0] == IntellifireErrorCode.FAN_DELAY
+            assert d.error_codes[1] == IntellifireErrorCode.OFFLINE
+
+            assert ", ".join([x.name for x in d.error_codes]) == "FAN_DELAY, OFFLINE"
+
+            assert (
+                d.error_codes_string
+                == ", ".join([x.name for x in d.error_codes])
+                == "FAN_DELAY, OFFLINE"
+            )
+
+            assert d.error_fan_delay is True
+            assert d.error_offline is True
+            assert d.error_accessory is False
+
+        except:  # noqa: E722
+            self.fail("Couldn't parse D1")
+
+        try:
+            IntellifirePollData.parse_obj(d2)
+        except:  # noqa: E722
+            self.fail("Couldn't parse D2")
