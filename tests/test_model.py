082e88d0d6408dd90ee8a7d3d1b9c1b11163fc68
Jeef <jeeftor@users.noreply.github.com>
2022-06-03 11:59:29 -0600

    tacos

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,4 +1,6 @@
 """Test File."""
+import os
+
 from pydantic import ValidationError
 
 from intellifire4py import IntellifirePollData
@@ -6,7 +8,8 @@ from intellifire4py import IntellifirePollData
 
 def test_json_files() -> None:
     """Test Function."""
-    for file_name in ["test1.json", "error_6.json"]:
+    base_path = os.path.dirname(os.path.realpath(__file__))
+    for file_name in [base_path + "/test1.json", base_path + "/error_6.json"]:
         file = open(file_name)
         json_str = file.read()
         try:
